a864aa899b64b279aead981900f907ff0da88d6f
Aiden22222290 <amalt0826@gmail.com>
2025-09-04 21:33:36 +0000

    fuel

--- main.cpp
+++ main.cpp
@@ -1,12 +1,24 @@
 // Assignment 1 - Fuel Range
-// TODO: YOUR NAME HERE
+// Aiden Maltese
 #include <iostream>
-// TODO: Add the "using" statement
+using namespace std;
 
 int main() {
   // TODO: Declare your variables, with comments for each
-
+  double mpg = 30.77;
+  double gallons = 4.87;
+  double maxFill = 18;
+  double percentFilled = (gallons/maxFill)*100;
+  double milesLeft;
   // TODO: Prompt for and input the required values
+
+  milesLeft = gallons*mpg;
+
+  cout << "The vehicle can be driven " << milesLeft << " miles on the remaining fuel." << endl;
+  cout << "The vehicle has " << percentFilled << "% of the tank filled.";
+
+
+
 
   // TODO: Calculate the results
 
